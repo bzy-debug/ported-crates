4ccf9957c741b42b9d6d081cf938a1028960edbd
Zhou-Yicheng <11810925@mail.sustech.edu.cn>
2021-12-11 22:41:54 +0800

    port wasmtime-cranelift with SgxMutex

---
+++ wasmtime-cranelift-sgx/src/debug.rs
@@ -0,0 +1,21 @@
+//! Debug utils for WebAssembly using Cranelift.
+
+#![allow(clippy::cast_ptr_alignment)]
+use std::prelude::v1::*;
+
+/// Memory definition offset in the VMContext structure.
+#[derive(Debug, Clone)]
+pub enum ModuleMemoryOffset {
+    /// Not available.
+    None,
+    /// Offset to the defined memory.
+    Defined(u32),
+    /// Offset to the imported memory.
+    Imported(u32),
+}
+
+pub use write_debuginfo::{emit_dwarf, DwarfSection, DwarfSectionRelocTarget};
+
+mod gc;
+mod transform;
+mod write_debuginfo;
